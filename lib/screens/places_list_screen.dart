abddcfdfd92bdce67b3f6c1f2c6a2fb2b1d32f78
DeleMike <akindelemichael65@gmail.com>
2020-12-09 17:01:09 +0100

    initial commit

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/add_place_screen.dart';
+import '../providers/great_places.dart';
+import '../screens/place_detail_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your places'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+              }),
+        ],
+      ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: Text('Got no places, start adding places!'),
+                ),
+                builder: (_, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemBuilder: (ctx, i) => InkWell(
+                          onTap: () {},
+                          child: ListTile(
+                            leading: CircleAvatar(
+                                backgroundImage:
+                                    FileImage(greatPlaces.items[i].image)),
+                            title: Text(greatPlaces.items[i].title),
+                            subtitle:
+                                Text(greatPlaces.items[i].location.address),
+                            onTap: () {
+                              // go to detail page
+                              Navigator.of(context).pushNamed(
+                                  PlaceDetailScreen.routeName,
+                                  arguments: greatPlaces.items[i].id);
+                            },
+                          ),
+                        ),
+                        itemCount: greatPlaces.items.length,
+                      ),
+              ),
+      ),
+    );
+  }
+}
